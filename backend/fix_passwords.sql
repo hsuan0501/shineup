e73c3747a69eb74bd1927ca15e392bc8adff1b92
Hoony0529 <larroy850501@gmail.com>
2025-12-17 15:31:54 +0800

    chore: 1217清理專案並改善安全設定

--- backend/fix_passwords.sql
+++
@@ -1,4 +0,0 @@
--- Fix passwords to Qwe1234 (BCrypt hash)
-UPDATE users SET password='$2b$10$m8lm348q0VWqPuxXOdEfSOqvnvbAh2rEkLYi8/JP8q5nqNnEfC2Oq' WHERE id=1;
-UPDATE users SET password='$2b$10$m8lm348q0VWqPuxXOdEfSOqvnvbAh2rEkLYi8/JP8q5nqNnEfC2Oq' WHERE id=2;
-UPDATE users SET password='$2b$10$m8lm348q0VWqPuxXOdEfSOqvnvbAh2rEkLYi8/JP8q5nqNnEfC2Oq' WHERE id=3;
